eb5eb5e2acbae48a20d0333aaa921bd83006ab63
Abdulrauf0767 <araof1803@mail.com>
2025-05-12 13:10:03 +0500

    set text

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { DummyUser } from '../../public/DummyUser'
 
 const Admin = () => {
+    const user = DummyUser ;
   return (
     <div>
-      <h1 className='text-center font-title text-2xl mt-10'>Welcome to Admin page</h1>
+      <h1 className='text-center font-title text-2xl mt-10'>Welcome to Admin</h1>
     </div>
   )
 }
--- src/Pages/ShopOwner.jsx
+++ src/Pages/ShopOwner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { DummyUser } from '../../public/DummyUser'
 const ShopOwner = () => {
   return (
     <div>
-      <h1 className='text-center font-title text-2xl mt-10'>Welcome to Admin page</h1>
+      <h1 className='text-center font-title text-2xl mt-10'>Welcome to ShopOwner</h1>
     </div>
   )
 }
